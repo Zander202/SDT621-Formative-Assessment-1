4e0e666b8f30bc373d36cd66eea1abbe8a91a677
dzand <dzand@ZANDERSPC>
2026-04-23 11:51:51 +0200

    Done Section A Question 2

--- SectionA_Q2_ATMSystem/ATMSystem/ATMSystem/Program.cs
+++ SectionA_Q2_ATMSystem/ATMSystem/ATMSystem/Program.cs
@@ -10,6 +10,75 @@ namespace ATMSystem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("===== CTU Simple Atm System =====");
+            Console.WriteLine();
+
+
+            //Get user name
+            Console.Write("Hi ,What is your name? ");
+            string name = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+
+            //Welcome message
+            Console.WriteLine($"Welcome {name}!");
+
+            //Get and validate account balance
+            double balance = 0;
+            while (true)
+            {
+                Console.Write("Enter account balance: ");
+                if (double.TryParse(Console.ReadLine(), out balance))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a numeric value");
+                }
+                {
+
+                }
+            }
+            //Get and validate withdrawal amount
+            double withdrawalAmount = 0;
+            while (true)
+            {
+                Console.Write("Enter withdrawal amount: ");
+                if (double.TryParse(Console.ReadLine(), out withdrawalAmount))
+                {
+                    if (withdrawalAmount > balance)
+                    {
+                        Console.WriteLine("Insufficient funds. Please enter a smaller amount.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a numeric value");
+                }
+
+            }
+            //Calculate and Update balance
+            double updatedBalance = balance - withdrawalAmount;
+            string timestamp = DateTime.Now.ToString("dd MMM yyyy HH:mm:ss");
+
+            //Display Results
+            Console.WriteLine();
+            Console.WriteLine($"Withdrawal successful!");
+            Console.WriteLine($"Updated Balance: {updatedBalance:F2}");
+            Console.WriteLine($"Transaction Time: {timestamp}");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
+
         }
+
+
+
+
     }
 }
